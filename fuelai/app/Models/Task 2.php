4d3ccbcccf8603f61a28e134237f93c306d42d48
yahirpaz1102 <yfpazardon@uncg.edu>
2025-09-30 21:54:53 -0400

    Revert "60% backend additions"
    This reverts commit d50fcabfaa1f6c858c93472250501ad1defee917.

---
+++ fuelai/app/Models/Task 2.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tasks';
+
+    protected $primaryKey = 'task_id';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id',
+        'content',
+        'difficulty',
+        'category',
+        'is_completed',
+    ];
+
+    protected $casts = [
+        'is_completed' => 'boolean',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    protected $hidden = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
