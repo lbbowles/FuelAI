161e8c844e00bd4793fd443dd2022b0585300226
logan bowles <lbbowles2@uncg.edu>
2025-10-16 13:34:49 -0400

    Add nixpacks.toml for Railway deployment

--- MobileFuelAI/starter-for-ios/Sources/extensions/View+Background.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-/// A view modifier extension to easily apply a checkered background to any view.
-extension View {
-    func addCheckeredBackground() -> some View {
-        self.modifier(CheckeredModifier())
-    }
-}
