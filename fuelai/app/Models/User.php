9f6b873e8d84ad9c2ab76a11b4f58ce082de84a2
logan bowles <lbbowles2@uncg.edu>
2025-10-20 19:40:08 -0400

    Add password to fillable attributes so it would work with Mobile / Expo API calls for registration

--- fuelai/app/Models/User.php
+++ fuelai/app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'username',
         'email',
+        'password',
         'password_hash', //Since we are hashing we need this to be hash
         'role',
         'profile_image_url',
