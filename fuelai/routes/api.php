1f302bf913c4db86da865419bda4ac9bebe14ee3
logan bowles <lbbowles2@uncg.edu>
2025-10-21 17:54:51 -0400

    Added API forum creation endpoint

--- fuelai/routes/api.php
+++ fuelai/routes/api.php
@@ -4,13 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\UserController;
-
+use App\Http\Controllers\Api\V1\ForumController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+// API forum creation endpoints
 Route::get('/users/{user}/followers', [UserController::class, 'followers']);
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/users/{user}/follow', [UserController::class, 'follow']);
+    Route::post('/forums', [ForumController::class, 'store']);
 });
