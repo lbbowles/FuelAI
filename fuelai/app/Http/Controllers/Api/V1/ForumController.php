130e8e85ed23df5c84b79edcadf7c4c673c6542f
logan bowles <lbbowles2@uncg.edu>
2025-10-21 21:26:22 -0400

    Getting a 500 error after dropping a column.  Trying to diagnose it

--- fuelai/app/Http/Controllers/Api/V1/ForumController.php
+++ fuelai/app/Http/Controllers/Api/V1/ForumController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ForumController extends Controller
 {
